4d2adf007c674021abc7d8aaa2e7e8bd3afcd69f
waterleemark <116476047+waterleemark@users.noreply.github.com>
2022-11-12 10:38:15 +0700

    edit ../

--- View/dashboard.php
+++ View/dashboard.php
@@ -10,7 +10,7 @@
     <!-- <link rel="stylesheet" href="assets/css/login.css"> -->
     <title>dashboard</title>
     <!-- link css -->
-    <link rel="stylesheet" href="assets/css/form.css">
+    <link rel="stylesheet" href="../assets/css/form.css">
 </head>
 
 <body id="body-pd">
@@ -18,34 +18,34 @@
         <nav class="nav">
             <div>
                 <div class="nav__brand">
-                    <img src="assets/img/icons8-menu-24 (1).png" name="" class="nav__toggle" id="nav-toggle"></img>
+                    <img src="../assets/img/icons8-menu-24 (1).png" name="" class="nav__toggle" id="nav-toggle"></img>
                     <a href="#" class="nav__logo"></a>
                 </div>
 
                 <div class="nav__list">
                     <div class="nav__link collapse user">
-                        <img src="assets/img/icons8-user-24.png" name="" class="nav__icon"></img>
+                        <img src="../assets/img/icons8-user-24.png" name="" class="nav__icon"></img>
                         <span class="nav__name">Nama</span>
                     </div>
                     <a href="#" class="nav__link ">
-                        <img src="assets/img/icons8-home-page-24 (1).png" name="" class="nav__icon"></img>
+                        <img src="../assets/img/icons8-home-page-24 (1).png" name="" class="nav__icon"></img>
                         <span class="nav__name">Dashboard</span>
                     </a>
                     <div class="nav__link collapse">
-                        <img src="assets/img/icons8-health-book-24.png" name="" class="nav__icon"></img>
+                        <img src="../assets/img/icons8-health-book-24.png" name="" class="nav__icon"></img>
                         <span class="nav__name">Pasien</span>
                     </div>
                     <!-- <div class="nav__link collapse"> -->
                     <a href="antrian1.html" class="nav__link ">
-                        <img src="assets/img/icons8-id-card-24.png" name="" class="nav__icon"></img>
+                        <img src="../assets/img/icons8-id-card-24.png" name="" class="nav__icon"></img>
                         <span class="nav__name">Pendaftaran</span>
                     </a>
                     <!-- </div> -->
                     <div class="nav__link collapse">
-                        <img src="assets/img/icons8-treatment-24.png" name="" class="nav__icon"></img>
+                        <img src="../assets/img/icons8-treatment-24.png" name="" class="nav__icon"></img>
                         <span class="nav__name">Pemeriksaan</span>
 
-                        <img src="assets/img/icons8-expand-arrow-24 (1).png" name="" class="collapse__link"></img>
+                        <img src="../assets/img/icons8-expand-arrow-24 (1).png" name="" class="collapse__link"></img>
 
                         <ul class="collapse__menu">
                             <span><a href="kunjunganPasien.html" class="collapse__sublink">pasien baru</a></span><br>
@@ -54,11 +54,11 @@
                         </ul>
                     </div>
                     <div class="nav__link collapse">
-                        <img src="assets/img/icons8-file-24 (1).png" name="" class="nav__icon"></img>
+                        <img src="../assets/img/icons8-file-24 (1).png" name="" class="nav__icon"></img>
                         <span class="nav__name">Koding</span>
                     </div>
                     <div class="nav__link collapse">
-                        <img src="assets/img/icons8-folder-24 (1).png" name="" class="nav__icon"></img>
+                        <img src="../assets/img/icons8-folder-24 (1).png" name="" class="nav__icon"></img>
                         <span class="nav__name">Pelaporan</span>
                     </div>
                     <!-- <a href="#" class="nav__link">
@@ -68,7 +68,7 @@
                 </div>
             </div>
             <a href="login.html" class="nav__link">
-                <img src="assets/img/icons8-logout-24.png" name="" class="nav__icon"></img>
+                <img src="../assets/img/icons8-logout-24.png" name="" class="nav__icon"></img>
                 <span class="nav__name">Log Out</span>
             </a>
         </nav>
@@ -87,7 +87,7 @@
                         </marquee>
                     </td>
                     <td>
-                        <img src="assets/img/logo.png" alt="logo PYB">
+                        <img src="../assets/img/logo.png" alt="logo PYB">
 
                     </td>
                 </tr>
@@ -102,10 +102,10 @@
 <<<<<<< HEAD:view/dashboard.html
             <img src="../assets/img/logo puskesmas.png" alt="">
 =======
-            <img src="assets/img/logo puskesmas.png" alt="">
+            <img src="../assets/img/logo puskesmas.png" alt="">
 >>>>>>> 1c8bbf1bea1d87a6faf4260464fa8d385e208ae5:view/dashboard.php
         </div>
-        <script src="assets/js/sidebar.js"></script>
+        <script src="../assets/js/sidebar.js"></script>
 </body>
 
 </html>
