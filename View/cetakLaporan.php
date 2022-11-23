a2bb8b3b64671b6cac4944e17544b66d048a24fa
waterleemark <116476047+waterleemark@users.noreply.github.com>
2022-11-23 09:08:34 +0700

    Create cetakLaporan.php

---
+++ View/cetakLaporan.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table,
+        td {
+            border: 1px solid;
+            border-collapse: collapse;
+            justify-content: center;
+        }
+
+        td {
+            padding: 1em;
+        }
+
+        td h2,
+        td span {
+            text-align: center;
+        }
+
+        table tr td span .tittle{
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td>
+                <img src="../assets/img/logo.png" width="80px" alt="">
+            </td>
+            <td>
+                <h2>KLINIK POLITEKNIK YAKPERMAS</h2>
+                <span>Jl. Raya Jompo Kulon, Kec. Sokaraja, Kab. Banyumas 53181 Telp(0281) 6595816 <br></span>
+                <span>Website: https://politeknikyakpermas.ac.id/ email: info@politeknikyakpermas.ac.id</span>
+            </td>
+
+        </tr>
+        <tr>
+            <td colspan="4">
+                <span class="tittle">DAFTAR 10 BESAR PENYAKIT BULAN X TAHUN XXXX</span>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                <table>
+                    <tr>
+                        <td>No</td>
+                        <td>Kode ICD-10</td>
+                        <td>Nama Penyakit</td>
+                        <td>Jumlah</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>E10.9</td>
+                        <td>Diabetes Mellitus Non Insulin</td>
+                        <td>10</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
